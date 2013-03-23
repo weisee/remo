213f6ec76a948e493cef0704db828b527162cfa5
Tako Neko <oct8cat@gmail.com>
2013-03-23 10:21:50 +0400

    Mongoose connection tests added.

---
+++ test.js
@@ -0,0 +1,41 @@
+var should = require('should')
+  , express = require('express')
+
+describe('remo', function() {
+  var remo = require('./lib/remo')
+    , app = express()
+
+  it('should have "serve" method', function() {
+    remo.serve.should.be.a('function')
+  })
+
+  describe('#serve()', function() {
+    it('should throw an error if no mongoose/mongooseUri options provided', function() {
+      (function() {
+        remo.serve(app)
+      }).should.throwError(/mongoose.*mongooseUri/)
+    })
+    it('should not throw an error if mongoose/mongooseUri options provided', function() {
+      (function() {
+        remo.serve(app, {mongooseUri: 'mongodb://localhost/test'})
+        remo.serve(app, {mongoose: {}})
+      }).should.not.throwError(/mongoose.*mongooseUri/)
+    })
+    it('should throw an error if "mongooseUri" option isn\'t a string', function() {
+      (function() {
+        remo.serve(app, {mongooseUri: {}})
+      }).should.throwError(/mongooseUri.*string/)
+    })
+    it('should throw an error if "mongoose" option haven\'t "connection" property', function() {
+      (function() {
+        remo.serve(app, {mongoose: {}})
+      }).should.throwError(/Mongoose instance.*connection/)
+    })
+    it('should throw an error on mongoose connection error', function() {
+      (function() {
+        remo.serve(app, {mongooseUri: 'mongodb://nonexisthost/test'})
+      }).should.throwError(/Mongoose connection error/)
+    })
+  })
+
+})
