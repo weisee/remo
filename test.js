bc50de8a6610057f3d594b3922441e4731430463
Tako Neko <oct8cat@gmail.com>
2013-03-23 12:51:16 +0400

    test deps moved to "devDependencies"

--- test.js
+++ test.js
@@ -10,27 +10,32 @@ describe('remo', function() {
   })
 
   describe('#serve()', function() {
+
     it('should throw an error if no mongoose/mongooseUri options provided', function() {
       (function() {
         remo.serve(app)
       }).should.throwError(/mongoose.*mongooseUri/)
     })
+
     it('should not throw an error if mongoose/mongooseUri options provided', function() {
       (function() {
         remo.serve(app, {mongooseUri: 'mongodb://localhost/test'})
         remo.serve(app, {mongoose: {}})
       }).should.not.throwError(/mongoose.*mongooseUri/)
     })
+
     it('should throw an error if "mongooseUri" option isn\'t a string', function() {
       (function() {
         remo.serve(app, {mongooseUri: {}})
       }).should.throwError(/mongooseUri.*string/)
     })
+
     it('should throw an error if "mongoose" option haven\'t "connection" property', function() {
       (function() {
         remo.serve(app, {mongoose: {}})
       }).should.throwError(/Mongoose instance.*connection/)
     })
+
     it('should throw an error on mongoose connection error', function() {
       (function() {
         remo.serve(app, {mongooseUri: 'mongodb://nonexisthost/test'})
